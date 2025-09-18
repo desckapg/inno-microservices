39fcea72356ea952a29df34336e602b88debf84c
desckapg <denissapezko748@gmail.com>
2025-09-18 23:31:10 +0300

    feat(DAO): implement repositories for entities

---
+++ user-service/src/main/java/com/innowise/userservice/repository/CardRepository.java
@@ -0,0 +1,13 @@
+package com.innowise.userservice.repository;
+
+import com.innowise.userservice.model.entity.Card;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface CardRepository extends JpaRepository<Card, Long> {
+
+  @Query(value = "SELECT * FROM cards_info ci WHERE ci.id IN (:ids)", nativeQuery = true)
+  List<Card> findAllByIdIn(List<Long> ids);
+
+}
