94f34890602f6469bd46b395eb3192633ae74a86
desckapg <denissapezko748@gmail.com>
2025-09-23 11:05:43 +0300

    build[srping-boot]: change spring boot version to 4.0.0-M3 to use Jackson 3

--- user-service/build.gradle.kts
+++ user-service/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     java
-    id("org.springframework.boot") version "4.0.0-M2"
+    id("org.springframework.boot") version "4.0.0-M3"
     id("io.spring.dependency-management") version "1.1.7"
 }
 
@@ -28,7 +28,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
-
+    implementation("org.springframework.boot:spring-boot-starter-json")
 
     compileOnly("org.projectlombok:lombok")
     annotationProcessor("org.projectlombok:lombok")
@@ -37,7 +37,6 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-cache")
     implementation("org.springframework.boot:spring-boot-starter-data-redis")
-    testImplementation("org.springframework.restdocs:spring-restdocs-mockmvc")
     annotationProcessor("org.mapstruct:mapstruct-processor:1.6.3")
 
     implementation("org.springframework.boot:spring-boot-starter-liquibase")
