c3a1a29701d518c36cf3197e403be0a80913a0e8
desckapg <denissapezko748@gmail.com>
2025-09-23 22:02:27 +0300

    feat(controller): add more processed exception to global controller handler

--- user-service/src/main/java/com/innowise/userservice/handler/GlobalExceptionHandler.java
+++ user-service/src/main/java/com/innowise/userservice/handler/GlobalExceptionHandler.java
@@ -1,24 +1,56 @@
 package com.innowise.userservice.handler;
 
+import com.innowise.userservice.exception.CardAlreadyExistsException;
 import com.innowise.userservice.exception.CardNotFoundException;
+import com.innowise.userservice.exception.UserAlreadyExistsException;
 import com.innowise.userservice.exception.UserNotFoundException;
+import lombok.extern.slf4j.Slf4j;
+import org.jspecify.annotations.Nullable;
 import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+@Slf4j
 @ControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
   @ExceptionHandler(value = {RuntimeException.class})
-  public ResponseEntity<String> handleRuntimeException(RuntimeException ex) {
-    return ResponseEntity.internalServerError().body(ex.getMessage());
+  public ResponseEntity<Void> handleRuntimeException(RuntimeException ex) {
+    log.error(ex.getMessage(), ex);
+    return ResponseEntity.internalServerError().build();
   }
 
   @ExceptionHandler(value = {UserNotFoundException.class, CardNotFoundException.class})
-  public ResponseEntity<String> handleNotFoundException(RuntimeException ex) {
+  public ResponseEntity<Void> handleNotFoundException(RuntimeException ex) {
     return ResponseEntity.notFound().build();
+  }
+
+  @ExceptionHandler(value = {UserAlreadyExistsException.class, CardAlreadyExistsException.class})
+  public ResponseEntity<String> handleAlreadyExistsExceptions(RuntimeException ex) {
+    return ResponseEntity.status(HttpStatus.CONFLICT)
+        .body(ex.getMessage());
+  }
+
+  @Override
+  protected ResponseEntity<Object> handleMethodArgumentNotValid(
+      MethodArgumentNotValidException ex,
+      HttpHeaders headers,
+      HttpStatusCode status,
+      WebRequest request) {
+    return ResponseEntity.unprocessableContent()
+        .body(ex.getFieldErrors()
+            .stream()
+            .map(error -> error.getField() + " : " + error.getDefaultMessage())
+            .toList()
+        );
   }
 
 }
