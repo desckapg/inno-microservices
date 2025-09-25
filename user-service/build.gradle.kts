e7b00f6fe24eb99cbfba0263d6276371ad4ee4d4
desckapg <denissapezko748@gmail.com>
2025-09-25 09:17:44 +0300

    build: restructure Gradle setup with new multi-project configuration and update dependencies

--- user-service/build.gradle.kts
+++ user-service/build.gradle.kts
@@ -1,63 +1,46 @@
 plugins {
-    java
     id("org.springframework.boot") version "4.0.0-M3"
     id("io.spring.dependency-management") version "1.1.7"
 }
 
-group = "com.innowise"
 version = "0.0.1-SNAPSHOT"
 description = "user-service"
 
-java {
-    toolchain {
-        languageVersion = JavaLanguageVersion.of(21)
-    }
-}
-
-configurations {
-    compileOnly {
-        extendsFrom(configurations.annotationProcessor.get())
-    }
-}
-
-repositories {
-    mavenCentral()
-}
-
+val mockitoAgent = configurations.create("mockitoAgent")
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-json")
-
-    compileOnly("org.projectlombok:lombok")
-    annotationProcessor("org.projectlombok:lombok")
-
-    implementation("org.mapstruct:mapstruct:1.6.3")
-    annotationProcessor("org.mapstruct:mapstruct-processor:1.6.3")
-
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-cache")
     implementation("org.springframework.boot:spring-boot-starter-data-redis")
-
     implementation("org.springframework.boot:spring-boot-starter-liquibase")
-    developmentOnly("org.springframework.boot:spring-boot-devtools")
-    developmentOnly("org.springframework.boot:spring-boot-docker-compose")
-    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+    implementation(libs.mapstruct)
+
+    mockitoAgent(libs.mockito) { isTransitive = false }
+
+    compileOnly("org.projectlombok:lombok")
+
     runtimeOnly("org.postgresql:postgresql")
 
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+    developmentOnly("org.springframework.boot:spring-boot-docker-compose")
+
+    annotationProcessor("org.projectlombok:lombok")
+    annotationProcessor(libs.mapstruct.processor)
+    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+
+    testImplementation(libs.mockito)
     testImplementation("org.springframework.boot:spring-boot-testcontainers")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.testcontainers:postgresql")
     testImplementation("com.redis:testcontainers-redis")
     testImplementation("org.testcontainers:junit-jupiter")
+
     testCompileOnly("org.projectlombok:lombok")
     testAnnotationProcessor("org.projectlombok:lombok")
 }
 
 tasks.withType<Test> {
-    useJUnitPlatform()
-}
-
-tasks.withType<Wrapper> {
-    gradleVersion = "9.0"
+    //jvmArgs.add("-javaagent:${mockitoAgent.asPath}")
 }
