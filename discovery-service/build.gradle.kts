bb6b5a369a3d6d8adf01273642f1796e611f4e44
desckapg <denissapezko748@gmail.com>
2026-01-12 23:35:48 +0300

    INFRA-17: update spring boot version to 4.0.1 in discovery service

--- discovery-service/build.gradle.kts
+++ discovery-service/build.gradle.kts
@@ -1,18 +1,18 @@
 plugins {
-    id("org.springframework.boot") version "4.0.0-M3"
+    id("org.springframework.boot") version "4.0.1"
     id("io.spring.dependency-management") version "1.1.7"
 }
 
-version = "0.0.1-SNAPSHOT"
-
-extra["springCloudVersion"] = "2025.1.0-M3"
+extra["springCloudVersion"] = "2025.1.0"
 
 dependencies {
     implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-server")
 
     developmentOnly("org.springframework.boot:spring-boot-docker-compose")
 
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.boot:spring-boot-starter-test") {
+        exclude(group = "org.mockito", module = "mockito-core")
+    }
 
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
